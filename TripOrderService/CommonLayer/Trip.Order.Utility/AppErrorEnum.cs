949625603562a09a96d28300a2ed5e8f62c64726
dotnet-arch-system <181665177@qq.com>
2017-10-14 17:58:22 +0800

    updated

--- TripOrderService/CommonLayer/Trip.Order.Utility/AppErrorEnum.cs
+++ TripOrderService/CommonLayer/Trip.Order.Utility/AppErrorEnum.cs
@@ -13,27 +13,27 @@ namespace Trip.Order.Utility
         /// <summary>
         /// 成功
         /// </summary>
-        [DescriptionAttribute("成功")]
+        [Description("成功")]
         Success = 200,
         /// <summary>
         /// 请求要求身份验证
         /// </summary>
-        [DescriptionAttribute("请求要求身份验证")]
+        [Description("请求要求身份验证")]
         UnAuthorized = 401,
         /// <summary>
         /// 服务器内部错误
         /// </summary>
-        [DescriptionAttribute("服务器内部错误")]
+        [Description("服务器内部错误")]
         InternalServerError = 500,
         /// <summary>
         /// 已下单出现价格变动
         /// </summary>
-        [DescriptionAttribute("已下单出现价格变动")]
+        [Description("已下单出现价格变动")]
         OrderCreateChangePrice = 901,
         /// <summary>
         /// 验舱失败
         /// </summary>
-        [DescriptionAttribute("验舱失败")]
+        [Description("验舱失败")]
         CheckClassFailed = 902
     }
 
